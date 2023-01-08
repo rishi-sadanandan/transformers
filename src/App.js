c1c4b94cd8c863fa030bedceaeca8fbd73a86b60
Rishikesh Sadanandan <rishikesh.sadanandan@gmail.com>
2023-01-08 18:57:04 -0500

    adding ui for sending text, not functional

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = formData.get("name");
+    console.log(data);
+    // send data to amplify api and get response
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +25,11 @@ function App() {
         >
           Learn React
         </a>
+        <br />
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="name" />
+          <input type="submit" value="Submit" />
+        </form>
       </header>
     </div>
   );
